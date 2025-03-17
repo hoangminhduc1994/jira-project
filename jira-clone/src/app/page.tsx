3eaba7078e39ab537fb80a20d4b1494f63bca3fb
hoangminhduc1994 <151418180+hoangminhduc1994@users.noreply.github.com>
2025-03-17 08:15:20 +0700

    update-auth-last

--- jira-clone/src/app/page.tsx
+++ jira-clone/src/app/page.tsx
@@ -1,31 +1,20 @@
 
 
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
+import { getCurrent } from "@/features/auth/actions";
+import { UserButton } from "@/features/auth/components/user-button";
+import { redirect } from "next/navigation";
+
+
+
+export default async function Home() {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
   return (
-    <div className="flex gap-4">
-      <Button>
-        Primary
-      </Button>
-      <Button variant="secondary">
-        Secondary
-      </Button>
-      <Button variant="destructive">
-        Destructive
-      </Button>
-      <Button variant="ghost">
-        Ghost
-      </Button>
-      <Button variant="muted">
-        Muted
-      </Button>
-      <Button variant="outline">
-        Outline
-      </Button>
-      <Button variant="teritary">
-        Teritary
-      </Button>
+    <div>
+      <UserButton/>
     </div>
   );
 }
